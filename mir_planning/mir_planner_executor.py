0df87536dd2d21ab5fde0312528ef6e7dfdf39da
MannavaVivek <vivekmannava@gmail.com>
2023-12-07 10:06:23 +0100

    minor refactoring

--- mir_planning/mir_planner_executor.py
+++ mir_planning/mir_planner_executor.py
@@ -15,36 +15,44 @@ from actions.base_place_action import perform_place
 from actions.stage_action import perform_stage
 from actions.unstage_action import perform_unstage
 
-class PlannerExecutor(Node):
 
+class PlannerExecutor(Node):
     def __init__(self):
-        super().__init__('planner_executor')
+        super().__init__("planner_executor")
         up.shortcuts.get_environment().credits_stream = None
         self.pddl_reader = PDDLReader()
 
         self.execution_started = False
 
         domain_file = os.path.join(
-            get_package_share_directory('mir_planning'),
-            'config',
-            'domain.pddl'
-            )
-    
+            get_package_share_directory("mir_planning"), "config", "domain.pddl"
+        )
+
         problem_file = os.path.join(
-            get_package_share_directory('mir_planning'),
-            'config',
-            'smaller_problem.pddl'
-            )
+            get_package_share_directory("mir_planning"),
+            "config",
+            "smaller_problem.pddl",
+        )
 
-        self.declare_parameters(namespace="",
-                                parameters=[('domain_path', domain_file),
-                                            ('problem_path', problem_file)])
-        
-        self.domain_path = self.get_parameter('domain_path').get_parameter_value().string_value
-        self.problem_path = self.get_parameter('problem_path').get_parameter_value().string_value
+        self.declare_parameters(
+            namespace="",
+            parameters=[("domain_path", domain_file), ("problem_path", problem_file)],
+        )
 
-        self.problem = self.pddl_reader.parse_problem(self.domain_path, self.problem_path)
-        self.planner = OneshotPlanner(problem_kind=self.problem.kind, optimality_guarantee=PlanGenerationResultStatus.SOLVED_OPTIMALLY)
+        self.domain_path = (
+            self.get_parameter("domain_path").get_parameter_value().string_value
+        )
+        self.problem_path = (
+            self.get_parameter("problem_path").get_parameter_value().string_value
+        )
+
+        self.problem = self.pddl_reader.parse_problem(
+            self.domain_path, self.problem_path
+        )
+        self.planner = OneshotPlanner(
+            problem_kind=self.problem.kind,
+            optimality_guarantee=PlanGenerationResultStatus.SOLVED_OPTIMALLY,
+        )
 
         # create a dictionary of fluents and objects to update the initial states
         self.fluents_dict = {}
@@ -55,7 +63,7 @@ class PlannerExecutor(Node):
         for obj in self.problem.all_objects:
             self.objects_dict[str(obj.name)] = self.problem.object(f"{obj.name}")
 
-        self.failure_count = {}    
+        self.failure_count = {}
         self.goals = []
         self.removed_goals = []
 
@@ -70,12 +78,13 @@ class PlannerExecutor(Node):
         execution_count = 0
         while execution_count <= 3:
             sorted_goals = []
+            is_goals = 0
             for i in range(3):
                 is_goals, sorted_goals = self.check_for_goals()
                 if is_goals:
                     break
                 else:
-                    #TODO: should wait for some time?
+                    # TODO: should wait for some time?
                     self.get_logger().info(f"Try {i}: No goals to execute. retrying...")
 
             if is_goals:
@@ -83,7 +92,9 @@ class PlannerExecutor(Node):
                 self.execute(sorted_goals)
             else:
                 if self.execution_started:
-                    self.get_logger().info("All goals executed. Waiting for new goals...")
+                    self.get_logger().info(
+                        "All goals executed. Waiting for new goals..."
+                    )
                     execution_count += 1
                     if len(self.removed_goals) > 0:
                         self.goals = self.removed_goals
@@ -105,7 +116,7 @@ class PlannerExecutor(Node):
                 for i in range(len(self.goals)):
                     self.sorted_goals.append(self.goals[i])
             return (True, self.sorted_goals)
-        
+
     def remove_goal_with_object(self, object_name):
         for goal in self.problem.goals:
             goal_str = str(goal)
@@ -121,7 +132,7 @@ class PlannerExecutor(Node):
             if location in goal_str:
                 self.removed_goals.append(goal)
         self.goals = [goal for goal in self.goals if location not in self.removed_goals]
-        #TODO: doing it with removed goals for now. might cause issues in retries
+        # TODO: doing it with removed goals for now. might cause issues in retries
         self.problem.clear_goals()
 
     def remove_successful_goals(self):
@@ -132,21 +143,28 @@ class PlannerExecutor(Node):
         self.get_logger().info("Executing goals...")
         self.problem.clear_goals()
         for goal in goals:
-            self.get_logger().info(f'\t{goal}')
+            self.get_logger().info(f"\t{goal}")
             self.problem.add_goal(goal)
         self.plan = self.planner.solve(self.problem).plan.actions
         result = True
         for idx, action in enumerate(self.plan):
             action_name = action.action.name
-                
-            if action_name == "move_base": # TODO: need to organize this     better
+
+            if action_name == "move_base":  # TODO: need to organize this     better
                 result = perform_move_base(self, action)
             elif action_name == "perceive":
-                if idx+1 < len(self.plan) and (self.plan[idx + 1].action.name == "pick" or self.plan[idx + 1].action.name == "PICK"):
-                    pick_params = [str(params) for params in self.plan[idx + 1].actual_parameters]
+                if idx + 1 < len(self.plan) and (
+                    self.plan[idx + 1].action.name == "pick"
+                    or self.plan[idx + 1].action.name == "PICK"
+                ):
+                    pick_params = [
+                        str(params) for params in self.plan[idx + 1].actual_parameters
+                    ]
                     result = perform_perceive(self, action, pick_params[2])
                 else:
-                    result = perform_perceive(self, action, "mystery_object") # if it came to this, something went very wrong
+                    result = perform_perceive(
+                        self, action, "mystery_object"
+                    )  # if it came to this, something went very wrong
             elif action_name == "pick":
                 result = perform_pick(self, action)
             elif action_name == "stage_general" or action_name == "stage_large":
@@ -157,7 +175,7 @@ class PlannerExecutor(Node):
                 result = perform_place(self, action)
             elif action_name == "insert":
                 result = perform_insert(self, action)
-            
+
             if not result:
                 break
 
@@ -165,10 +183,9 @@ class PlannerExecutor(Node):
             self.remove_successful_goals()
 
 
-def main(args=None):
+def main():
     rclpy.init()
     planner_executor = PlannerExecutor()
     rclpy.spin(planner_executor)
     planner_executor.destroy_node()
     rclpy.shutdown()
-
